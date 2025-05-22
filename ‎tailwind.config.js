3ab1521fba9c50f740dfcd6087ce5eff766d336f
CatalystoEyes <xmajioff@gmail.com>
2025-05-22 21:56:09 +0300

    Add authentication

---
+++ ‎tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js"
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "black": "#060606"
+            },
+            spacing: {
+                '1': '100px',
+            },
+            fontFamily: {
+                "hanken-grotesk": ["Hanken Grotesk", "sans-serif"]
+            },
+            fontSize: {
+                "2xs": ".625rem" // 10px
+            }
+        },
+    },
+    plugins: [],
+}
